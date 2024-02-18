1bb127920a11eb58e07a3c489ba81a144db95f67
ilyaOlkh <143333462+ilyaOlkh@users.noreply.github.com>
2024-02-18 15:50:40 +0200

    Update prompt.js

--- app/service/prompt.js
+++ app/service/prompt.js
@@ -2,40 +2,41 @@ const url = 'http://api.pizza.ua/'
 
 
 export default async function prompt(currentFile, objectToSend) {
-    const urlLocal = url + currentFile;
+    // const urlLocal = url + currentFile;
 
-    try {
-        let response
-        if (objectToSend) {
-            response = await fetch(urlLocal, {
-                next: { revalidate: 30 },
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(objectToSend),
-            });
-        } else {
-            response = await fetch(urlLocal, {
-                next: { revalidate: 30 },
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-        }
+    // try {
+    //     let response
+    //     if (objectToSend) {
+    //         response = await fetch(urlLocal, {
+    //             next: { revalidate: 30 },
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //             },
+    //             body: JSON.stringify(objectToSend),
+    //         });
+    //     } else {
+    //         response = await fetch(urlLocal, {
+    //             next: { revalidate: 30 },
+    //             method: 'GET',
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //             },
+    //         });
+    //     }
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
+    //     if (!response.ok) {
+    //         throw new Error(`HTTP error! status: ${response.status}`);
+    //     }
 
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        if (error.message === 'Failed to fetch') {
-            console.log('Сервер не работает');
-        } else {
-            console.log('Произошла ошибка при проверке API:', error);
-        }
-    }
+    //     const data = await response.json();
+    //     return data;
+    // } catch (error) {
+    //     if (error.message === 'Failed to fetch') {
+    //         console.log('Сервер не работает');
+    //     } else {
+    //         console.log('Произошла ошибка при проверке API:', error);
+    //     }
+    // }
+    return []
 }
